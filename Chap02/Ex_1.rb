3371c88f033df5435f34ae8a1f61e97aabdf4e1c
Léa R <lea.rosamond@gmail.com>
2017-11-02 22:11:42 +0000

    Exercice 2

---
+++ Chap02/Ex_1.rb
@@ -0,0 +1,11 @@
+#Hours in a year
+puts 24 * 365
+
+#Hours in a year (leap)
+puts 24 * 366
+
+#Minutes in a decade
+puts ((60 * 24) * 365) * 10
+
+#Your age in seconds
+puts ((60*60) * 24 ) * 365 *24
