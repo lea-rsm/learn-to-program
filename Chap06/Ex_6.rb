fdb24d8b87e6c7aa173c660f750c7843609afc69
Léa R <lea.rosamond@gmail.com>
2017-11-03 11:26:59 +0000

    Exercice Chapitre 6

---
+++ Chap06/Ex_6.rb
@@ -0,0 +1,14 @@
+#Angry boss
+
+puts "What do you want?!"
+answer = gets.chomp
+puts "WHADDAYA MEAN " + answer.upcase.to_s + " YOU'RE FIRED!!!"
+
+#Table of contents
+
+line_width = 50
+puts ('Table of Contents'.center(line_width))
+puts ''
+puts 'Chapter 1: Getting Started'.ljust(30) + 'page 1'.rjust(20)
+puts 'Chapter 2: Numbers'.ljust(30) + 'page 2'.rjust(20)
+puts 'Chapter 3: Letters'.ljust(30) + 'page 3'.rjust(20)
