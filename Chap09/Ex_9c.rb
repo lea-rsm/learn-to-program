cfb0312521490ef058479905db1703e8ef26da03
Léa R <lea.rosamond@gmail.com>
2017-11-04 10:36:37 +0000

    Exercice C - à revoir

---
+++ Chap09/Ex_9c.rb
@@ -0,0 +1,44 @@
+#Modern Roman numerals
+
+def new_roman(num)
+
+thousands = (num / 1000)
+hundreds = (num % 1000 /100)
+tens = (num % 100 / 10)
+ones = (num % 10)
+
+roman = 'M' * thousands
+
+if hundreds == 9
+roman = roman + 'CM'
+elsif hundreds == 4
+  roman = roman + 'CD'
+else
+  roman = roman + 'D' * (num % 1000 / 500)
+  roman = roman + 'C' * (num % 500 / 100)
+end
+
+if tens == 9
+  roman = roman + 'XC'
+elsif tens == 4
+  roman = roman + 'XL'
+else
+  roman = roman + 'L' * (num % 100 / 50)
+  roman = roman + 'X' * (num % 50 / 10)
+end
+if ones == 9
+roman = roman + 'IX'
+elsif ones == 4
+  roman = roman + 'IV'
+else
+  roman = roman + 'V' * (num % 10/ 5)
+  roman = roman + 'I' * (num % 5/ 1)
+end
+roman
+
+end
+
+puts (new_roman(999))
+puts (new_roman(187))
+puts (new_roman(19361))
+puts (new_roman(1999))
