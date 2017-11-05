99f8282e077182ac986e087a7fed575727ffac0a
Léa R <lea.rosamond@gmail.com>
2017-11-05 12:20:08 +0000

    Expanded numbers

--- Chap10/ex_10c.rb
+++ Chap10/ex_10c.rb
@@ -18,7 +18,7 @@ def english_number number
 
     tens_place = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
-    zillions = [['hundred', 2], ['thousand', 3], ['million', 6], ['billion', 9], ['trillion', 12], ['quadrillion', 15], ['quintillion', 18], ['sextillion', 21]
+    zillions = [['hundred', 2], ['thousand', 3], ['million', 6], ['billion', 9], ['trillion', 12], ['quadrillion', 15], ['quintillion', 18], ['sextillion', 21],
                 ['septillion', 24], ['octillion', 27], ['nonillion', 30], ['decillion', 33], ['undecillion', 36], ['duodecillion', 39], ['tredecillion', 42],
                 ['quattuordecillion', 45], ['quindecillion', 48],
                 ['sexdecillion', 51], ['septendecillion', 54], ['octodecillion', 57], ['novemdecillion', 60], ['vigintillion', 63], ['googol', 100]]
